a8b9f5d3bfa279ca6bb77162384981e40f2c1be8
Jay Landro <jaylandro@users.noreply.github.com>
2021-02-06 12:31:57 -0800

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
+  const { name = 'Bobba' } = req.query;
   res.statusCode = 200
-  res.json({ name: 'John Doe', values: req })
+  res.json({ name })
 }
